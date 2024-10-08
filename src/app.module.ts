45573baab2fbf476fcfd1c1ea3eec2b5feb674cf
badr-alwattar <badr.alwattar@gmail.com>
2024-10-08 22:39:08 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,30 +11,28 @@ import { SearchResult } from './search/search-result.entity';
 
 @Module({
   imports: [
-  //   ConfigModule.forRoot({
-  //     isGlobal: true, // this makes the configuration available globally
-  //   }),
-  //   TypeOrmModule.forRootAsync({
-  //     imports: [ConfigModule],
-  //     inject: [ConfigService],
-  //     useFactory: (configService: ConfigService) => ({
-  //       type: 'postgres',
-  //       host: configService.get('RDS_HOSTNAME'),
-  //       port: parseInt(configService.get('RDS_PORT'), 10),
-  //       database: configService.get('RDS_DB_NAME'),
-  //       username: configService.get('RDS_USERNAME'),
-  //       password: configService.get('RDS_PASSWORD'),
-  //       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  //       synchronize: true,
-  //       // dropSchema: true,
-  //     }),
-  //   }),
-  //   TypeOrmModule.forFeature([Search, SearchResult]),
-  //   SearchModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // this makes the configuration available globally
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('RDS_HOSTNAME'),
+        port: parseInt(configService.get('RDS_PORT'), 10),
+        database: configService.get('RDS_DB_NAME'),
+        username: configService.get('RDS_USERNAME'),
+        password: configService.get('RDS_PASSWORD'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        // dropSchema: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([Search, SearchResult]),
+    SearchModule,
   ],
-  // controllers: [AppController, SearchController],
-  // providers: [AppService, SearchService],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, SearchController],
+  providers: [AppService, SearchService],
 })
 export class AppModule {}
